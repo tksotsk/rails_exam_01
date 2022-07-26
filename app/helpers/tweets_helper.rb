6cf9c0b8b4a45b42c36757ca46aad8f3891d7bc3
takashi otsuka <shoshi100010000@gmail.com>
2022-07-26 11:12:05 +0900

    Timezoneの編集

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -1,2 +1,9 @@
 module TweetsHelper
+    def sentence_choose_new_or_edit
+        if action_name == "new" || action_name == "create" 
+            "ツイートの作成"
+        elsif action_name == "edit"
+            "ツイートの編集"
+        end
+    end
 end
